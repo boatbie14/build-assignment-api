5d5b00d1dd54dc3e0cb9dc4e7d444e9d2d508bc8
Kittiphat <kittiphat.rpp@gmail.com>
2025-04-08 14:27:19 +0700

    feat(API): Add API for create an assignments

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,47 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
-app.get("/test", (req, res) => {
+app.use(express.json());
+
+app.get("/assignments", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.post("/assignments", async (req, res) => {
+  try {
+    const newAssignment = {
+      ...req.body,
+      created_at: new Date(),
+      updated_at: new Date(),
+      published_at: new Date(),
+    };
+
+    //console.log(newAssignment);
+
+    await connectionPool.query(
+      `INSERT INTO assignments (user_id, title, content, category, length, status, created_at, updated_at, published_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+      [
+        newAssignment.user_id,
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.length,
+        newAssignment.status,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at,
+      ]
+    );
+
+    return res.status(201).json({ message: "Created assignment sucessfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ message: "Server could not create assignment because database connection" });
+  }
 });
 
 app.listen(port, () => {
